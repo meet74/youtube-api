7484bc62d41617166a37d49092f6f950c9af43fc
meet74 <meet237@gmail.com>
2022-06-23 16:04:42 +0530

    Setting up GrifFs

--- index.js
+++ index.js
@@ -1,18 +1,88 @@
+//imports
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const multer = require('multer');
+const {GridFsStorage} = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+const path = require('path');
+const crypto = require('crypto');
+const db_url = require('./src/database');
 
+
+
+//Middlewares
+app.use(bodyParser.json());
+
+//creating server
 const server = require('http').createServer(app);
 
-const initializeDatabase = require('./src/database');
-
-
 //intializing Database
-initializeDatabase();
+const dbConnection = mongoose
+.createConnection(db_url)
 
+//Init gfs
+let gfs;
+
+
+//adding one time listener
+dbConnection.once('open', () => {
+    gfs = Grid(dbConnection.db,mongoose.mongo);
+    gfs.collection('uploads');
+})
+
+
+//creating storage
+
+const storage = new GridFsStorage({
+    url:db_url,
+    db:dbConnection,
+    file:(req,file) => {
+        return new Promise((res,rej) => {
+            crypto.randomBytes(16,(err,buf) => {
+                if (err) {
+                    return rej(err);
+                }
+                const fileName = buf.toString('hex')+path.extname(file.orignalname);
+                const fileInfo = {
+                    filename:fileName,
+                    bucketName:"uploads"
+                };
+                res(fileInfo)
+            })
+        })
+    }
+})
+
+//creating multer
+const uploads = multer({storage});
+
+app.post('/uploadvideo',uploads.single('file'),(req,res) => {
+    res.send('File uploaded');
+})
+
+//@Router Get request
 app.get('/',(req,res) => {
     res.send('Shreehari')
 })
 
+app.get('/files', (req, res) => {
+    gfs.files.find().toArray((err, files) => {
+      // Check if files
+      if (!files || files.length === 0) {
+        return res.status(404).json({
+          err: 'No files exist'
+        });
+      }
+  
+      // Files exist
+      return res.json(files);
+    });
+  });
 
 
+
+//starting server
 server.listen(5000,()=>console.log('server is listening on 5000'))
--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,5 @@
-const mongoose = require("mongoose");
 
-const db_url = "mongodb+srv://meetsn:meetsn@cluster0.keolo.mongodb.net/?retryWrites=true&w=majority";
 
-module.exports = initializeDatabase = () => {
-  mongoose
-    .connect(db_url)
-    .then(res => console.log('Database is connected'))
-    .catch(err => console.log(err));
-};
+
+
+module.exports = db_url =  "mongodb://localhost:27017";
